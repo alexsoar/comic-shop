46570552f1a52f6b2976fb655aaf6a0bfb4f24aa
Alex Soar <cop.soar@gmail.com>
2024-05-02 11:42:21 +0100

    Superland - comic shop

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="wrapper">
+      <header>
+        <div className="westSide">
+          <img width={150} src="/img/logo.jpg" alt="" />
+        </div>
+        <div className="headerInfo">
+          <h3>Comic Shop</h3>
+          <p>The best comic shop ever</p>
+        </div>
+        <ul className="eastSide">
+          <li>
+            <svg></svg>
+          </li>
+          <li>
+            <span>1280 £</span>
+          </li>
+          <li>
+            <svg></svg>
+          </li>
+          <li>
+            <span>favorites</span>
+          </li>
+          <li>
+            <svg></svg>
+          </li>
+          <li>
+            <span>profile</span>
+          </li>
+        </ul>
+      </header>
+      <div className="container">
+        <h1>All comics</h1>
+        ...
+      </div>
     </div>
   );
 }
