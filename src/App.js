326342dd144515b398f27e55c0455256f2d88eb3
Alex Soar <cop.soar@gmail.com>
2024-05-03 14:52:02 +0100

    Added more items and update css styles

--- src/App.js
+++ src/App.js
@@ -24,20 +24,119 @@ function App() {
           </li>
         </ul>
       </header>
+      <h1>All comics</h1>
       <div className="container">
-        <h1>All comics</h1>
-        ...
+        {/* Item cards START */}
         <div className="item">
           <img width={200} src="/img/posters/avengers.jpeg" alt="" />
           <h2>Avengers</h2>
-        </div>
-        <div className="itemBottom">
-          <div className="price">
-            <span>Price:</span>
-            <b>12£</b>
+          <div className="itemBottom">
+            <div className="price">
+              <span>Price:</span>
+              <p>12£</p>
+            </div>
+            <div className="buttons">
+              <button className="addButton">
+                <img src="/img/like-off.svg" alt="button" />
+              </button>
+              <button className="addButton">
+                <img src="/img/button.svg" alt="button" />
+              </button>
+            </div>
           </div>
-          <button className="addButton">+</button>
         </div>
+        <div className="item">
+          <img width={200} src="/img/posters/ironman.jpeg" alt="" />
+          <h2>Avengers</h2>
+          <div className="itemBottom">
+            <div className="price">
+              <span>Price:</span>
+              <p>12£</p>
+            </div>
+            <button className="addButton">
+              <img src="/img/button.svg" alt="button" />
+            </button>
+          </div>
+        </div>
+        <div className="item">
+          <img width={200} src="/img/posters/superman.jpeg" alt="" />
+          <h2>Avengers</h2>
+          <div className="itemBottom">
+            <div className="price">
+              <span>Price:</span>
+              <p>12£</p>
+            </div>
+            <button className="addButton">
+              <img src="/img/button.svg" alt="button" />
+            </button>
+          </div>
+        </div>
+        <div className="item">
+          <img width={200} src="/img/posters/spiderman.jpeg" alt="" />
+          <h2>Avengers</h2>
+          <div className="itemBottom">
+            <div className="price">
+              <span>Price:</span>
+              <p>12£</p>
+            </div>
+            <button className="addButton">
+              <img src="/img/button.svg" alt="button" />
+            </button>
+          </div>
+        </div>
+        <div className="item">
+          <img width={200} src="/img/posters/greenarrow.jpeg" alt="" />
+          <h2>Avengers</h2>
+          <div className="itemBottom">
+            <div className="price">
+              <span>Price:</span>
+              <p>12£</p>
+            </div>
+            <button className="addButton">
+              <img src="/img/button.svg" alt="button" />
+            </button>
+          </div>
+        </div>
+        <div className="item">
+          <img width={200} src="/img/posters/flash.jpeg" alt="" />
+          <h2>Avengers</h2>
+          <div className="itemBottom">
+            <div className="price">
+              <span>Price:</span>
+              <p>12£</p>
+            </div>
+            <button className="addButton">
+              <img src="/img/button.svg" alt="button" />
+            </button>
+          </div>
+        </div>
+        <div className="item">
+          <img width={200} src="/img/posters/nightthrasher.jpeg" alt="" />
+          <h2>Avengers</h2>
+          <div className="itemBottom">
+            <div className="price">
+              <span>Price:</span>
+              <p>12£</p>
+            </div>
+            <button className="addButton">
+              <img src="/img/button.svg" alt="button" />
+            </button>
+          </div>
+        </div>
+        <div className="item">
+          <img width={200} src="/img/posters/captainamerica.jpeg" alt="" />
+          <h2>Avengers</h2>
+          <div className="itemBottom">
+            <div className="price">
+              <span>Price:</span>
+              <p>12£</p>
+            </div>
+            <button className="addButton">
+              <img src="/img/button.svg" alt="button" />
+            </button>
+          </div>
+        </div>
+        {/* Item cards END */}
       </div>
     </div>
   );
