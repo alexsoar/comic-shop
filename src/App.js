999dd6eb1d35d57f5c09bfba558cce27b6b4d718
Alex Soar <cop.soar@gmail.com>
2024-06-05 13:17:19 +0100

    get data array using fetch from mockapi

--- src/App.js
+++ src/App.js
@@ -4,27 +4,37 @@ import { CardSidebar } from './components/CardSidebar/CardSidebar';
 import { Search } from './components/Search/Search';
 import React from 'react';
 
-const productList = [
-  { title: 'Avengers', imageUrl: '/img/posters/avengers.jpeg', price: 12 },
-  { title: 'Iron Man', imageUrl: '/img/posters/ironman.jpeg', price: 10 },
-  { title: 'Superman', imageUrl: '/img/posters/superman.jpeg', price: 11 },
-  { title: 'Spiderman', imageUrl: '/img/posters/spiderman.jpeg', price: 24 },
-  { title: 'Green Arrow', imageUrl: '/img/posters/greenarrow.jpeg', price: 19 },
-  { title: 'Flash', imageUrl: '/img/posters/flash.jpeg', price: 15 },
-  {
-    title: 'Night Thrasher',
-    imageUrl: '/img/posters/nightthrasher.jpeg',
-    price: 10,
-  },
-  {
-    title: 'Captain America',
-    imageUrl: '/img/posters/captainamerica.jpeg',
-    price: 20,
-  },
-];
-
+// const productList = [
+//   { title: 'Avengers', imageUrl: '/img/posters/avengers.jpeg', price: 12 },
+//   { title: 'Iron Man', imageUrl: '/img/posters/ironman.jpeg', price: 10 },
+//   { title: 'Superman', imageUrl: '/img/posters/superman.jpeg', price: 11 },
+//   { title: 'Spiderman', imageUrl: '/img/posters/spiderman.jpeg', price: 24 },
+//   { title: 'Green Arrow', imageUrl: '/img/posters/greenarrow.jpeg', price: 19 },
+//   { title: 'Flash', imageUrl: '/img/posters/flash.jpeg', price: 15 },
+//   {
+//     title: 'Night Thrasher',
+//     imageUrl: '/img/posters/nightthrasher.jpeg',
+//     price: 10,
+//   },
+//   {
+//     title: 'Captain America',
+//     imageUrl: '/img/posters/captainamerica.jpeg',
+//     price: 20,
+//   },
+// ];
 function App() {
   const [cartOpened, setCartOpened] = React.useState(false);
+  const [productList, setproductList] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://666043af5425580055b31258.mockapi.io/Items')
+      .then((result) => {
+        return result.json();
+      })
+      .then((json) => {
+        setproductList(json);
+      });
+  });
 
   return (
     <div className="wrapper">
