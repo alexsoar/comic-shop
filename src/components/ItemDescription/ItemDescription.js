fb37395cab718ed1ca0271afbe9efe51e601e61d
Alex Soar <cop.soar@gmail.com>
2024-09-09 13:50:33 +0100

    update

--- src/components/ItemDescription/ItemDescription.js
+++ src/components/ItemDescription/ItemDescription.js
@@ -12,24 +12,26 @@ function ItemDescription({
 }) {
   console.log('Props in ItemDescription:', { title, imageUrl });
   return (
-    <div
-      style={{
-        backgroundImage:
-          "linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9)), url('/img/posters/immortal-thor-desktop.jpg')",
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        width: '100%',
-        minWidth: '335px',
-        height: '500px',
-      }}
-      className={styles.itemDescriptionContainer}
-    >
-      <div>
-        <img width={250} src={imageUrl} alt="" />
-      </div>
-      <div>
-        <h1>{title}</h1>
-        <div style={{ color: '#fff' }}>{published}</div>
+    <div className={styles.glassEffect}>
+      <div
+        style={{
+          backgroundImage:
+            "linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9)), url('/img/posters/immortal-thor-desktop.jpg')",
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          width: '100%',
+          minWidth: '335px',
+          height: '500px',
+        }}
+        className={styles.itemDescriptionContainer}
+      >
+        <div>
+          <img width={250} src={imageUrl} alt="" />
+        </div>
+        <div>
+          <h1>{title}</h1>
+          <div style={{ color: '#fff' }}>{published}</div>
+        </div>
       </div>
     </div>
   );
